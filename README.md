a040b6785c583913988a03109710bc86f951dbde
jonatanalava <jalava@earlyshares.com>
2016-03-11 12:08:06 -0500

    First Commi

---
+++ README.md
@@ -0,0 +1,13 @@
+# Eager Wrapper for Copyhawk
+
+Copyhawk simplifies content management for small content.
+
+Setup Copyhawk using Eager. Add your token and default language into Eager' dashboard. You can find those on Copyhawk' script pages.
+After Copyhawk is setup all you have to do is to add Copyhawk tags wherever you want your content to appear.
+Copyhawk tags look like this: 
+
+`<c-hawk data-label="LABEL"></c-hawk>`
+
+Where LABEL is your specific script's label as defined by you on Copyhawk's UI.
+
+If you have any question, you can contact us at [admin@copyhawk.co](mailto:admin@copyhawk.co)
---
+++ eyas.js
@@ -0,0 +1,77 @@
+var copyHawkMaster;
+
+(function(){
+
+  var options = INSTALL_OPTIONS;
+
+  if (!options.token && !options.defaultLanguage) {
+    return;
+  }
+
+  var initTag = document.createElement('c-hawk-init');
+  initTag.setAttribute('data-site-token',options.token);
+  initTag.setAttribute('data-default-language',options.defaultLanguage);
+  initTag.setAttribute('data-supported-languages',options.supportedLanguages);
+  initTag.setAttribute('data-environment',options.environment);
+  document.body.appendChild(initTag);
+
+  copyHawkMaster = {
+    init: function() {
+      var lookup = document.getElementsByTagName('c-hawk-init')[0].dataset;
+      this.siteToken = lookup.siteToken;
+      this.language = lookup.defaultLanguage;
+      this.supportedLanguages = lookup.supportedLanguages;
+      if (lookup.environment == "preview") {
+        this.preview = true;
+      }
+      this.noCache = new Date().getTime();
+    }
+  }
+  copyHawkMaster.init();
+
+
+})();
+
+function getDefault(label) {
+  // Fail fast simple debug
+  return "[ch-error: missing label "+label+" ]";
+}
+
+function getPreviewText(token, label, lang, cacheKey, element) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      element.innerHTML = xhttp.responseText;
+    } else if (xhttp.readyState == 4 && xhttp.status == 404) {
+      element.innerHTML = getDefault(label);
+    }
+  }
+  xhttp.open("GET", "//app.copyhawk.co/api/p/staging." + token + "/" + label + "?lang="+lang+"&cache="+cacheKey, true);
+  xhttp.setRequestHeader("Content-Type", "text/plain");
+  xhttp.send();
+}
+
+function getLiveText(token, label, lang, cacheKey, element) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      element.innerHTML = xhttp.responseText;
+    } else if (xhttp.readyState == 4 && xhttp.status == 404) {
+      element.innerHTML = getDefault(label);
+    }
+  }
+  xhttp.open("GET", "//app.copyhawk.co/api/" + token + "/" + label + "?lang="+lang+"&cache="+cacheKey, true);
+  xhttp.setRequestHeader("Content-Type", "text/plain");
+  xhttp.send();
+}
+
+// Init script tags
+var cHawkProto = Object.create(HTMLElement.prototype);
+cHawkProto.createdCallback = function () {
+  if (copyHawkMaster.preview) {
+    getPreviewText(copyHawkMaster.siteToken, this.dataset.label, copyHawkMaster.language, copyHawkMaster.noCache, this);
+  } else {
+    getLiveText(copyHawkMaster.siteToken, this.dataset.label, copyHawkMaster.language, copyHawkMaster.noCache, this);
+  }
+};
+var cHawk = document.registerElement("c-hawk", {prototype: cHawkProto});
