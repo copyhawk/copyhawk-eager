df8f2576d2017b6ffb268f6cefeb5dbe949b1951
Zack Bloom <zackbloom@gmail.com>
2016-03-21 18:32:58 -0400

    Cleanup options and JS

--- eyas.js
+++ eyas.js
@@ -1,77 +1,66 @@
-var copyHawkMaster;
-
 (function(){
 
   var options = INSTALL_OPTIONS;
 
-  if (!options.token && !options.defaultLanguage) {
+  if (!options.token) {
     return;
   }
 
-  var initTag = document.createElement('c-hawk-init');
-  initTag.setAttribute('data-site-token',options.token);
-  initTag.setAttribute('data-default-language',options.defaultLanguage);
-  initTag.setAttribute('data-supported-languages',options.supportedLanguages);
-  initTag.setAttribute('data-environment',options.environment);
-  document.body.appendChild(initTag);
+  function getDefault(label) {
+    // Fail fast simple debug
+    return "[ch-error: missing label "+label+" ]";
+  }
 
-  copyHawkMaster = {
-    init: function() {
-      var lookup = document.getElementsByTagName('c-hawk-init')[0].dataset;
-      this.siteToken = lookup.siteToken;
-      this.language = lookup.defaultLanguage;
-      this.supportedLanguages = lookup.supportedLanguages;
-      if (lookup.environment == "preview") {
-        this.preview = true;
+  function getText(preview, token, label, lang, element) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+        element.innerHTML = xhttp.responseText;
+      } else if (xhttp.readyState == 4 && xhttp.status == 404) {
+        element.innerHTML = getDefault(label);
       }
-      this.noCache = new Date().getTime();
+    }
+
+    var url = "//app.copyhawk.co/api/";
+    if (preview)
+      url += 'p/staging.';
+    url += token + "/" + label + "?lang=" + lang + "&cache=" + Math.random().toString(36);
+
+    xhttp.open("GET", url, true);
+    xhttp.setRequestHeader("Content-Type", "text/plain");
+    xhttp.send();
+  }
+
+  function loadTags() {
+    var tags = document.querySelectorAll('c-hawk');
+
+    var preview = options.advancedOptions.environment === 'staging';
+
+    if (tags) {
+      for (var i=tags.length; i--;){
+        getText(preview, options.token, tags[i].dataset.label, options.advancedOptions.defaultLanguage, tags[i]);
+      }
     }
   }
-  copyHawkMaster.init();
 
+  var regionEls = [];
+  function writeTags() {
+    for (var i=options.regions.length; i--;){
+      var region = options.regions[i];
+
+      regionEls[i] = Eager.createElement(region.location, regionEls[i]);
+
+      var child = document.createElement('c-hawk');
+      child.dataset.label = region.label;
+      regionEls[i].appendChild(child);
+    }
+  }
+
+  function render() {
+    writeTags();
+    loadTags();
+  }
+
+  document.addEventListener('DOMContentLoaded', render);
 
 })();
-
-function getDefault(label) {
-  // Fail fast simple debug
-  return "[ch-error: missing label "+label+" ]";
-}
-
-function getPreviewText(token, label, lang, cacheKey, element) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      element.innerHTML = xhttp.responseText;
-    } else if (xhttp.readyState == 4 && xhttp.status == 404) {
-      element.innerHTML = getDefault(label);
-    }
-  }
-  xhttp.open("GET", "//app.copyhawk.co/api/p/staging." + token + "/" + label + "?lang="+lang+"&cache="+cacheKey, true);
-  xhttp.setRequestHeader("Content-Type", "text/plain");
-  xhttp.send();
-}
-
-function getLiveText(token, label, lang, cacheKey, element) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      element.innerHTML = xhttp.responseText;
-    } else if (xhttp.readyState == 4 && xhttp.status == 404) {
-      element.innerHTML = getDefault(label);
-    }
-  }
-  xhttp.open("GET", "//app.copyhawk.co/api/" + token + "/" + label + "?lang="+lang+"&cache="+cacheKey, true);
-  xhttp.setRequestHeader("Content-Type", "text/plain");
-  xhttp.send();
-}
-
-// Init script tags
-var cHawkProto = Object.create(HTMLElement.prototype);
-cHawkProto.createdCallback = function () {
-  if (copyHawkMaster.preview) {
-    getPreviewText(copyHawkMaster.siteToken, this.dataset.label, copyHawkMaster.language, copyHawkMaster.noCache, this);
-  } else {
-    getLiveText(copyHawkMaster.siteToken, this.dataset.label, copyHawkMaster.language, copyHawkMaster.noCache, this);
-  }
-};
-var cHawk = document.registerElement("c-hawk", {prototype: cHawkProto});
